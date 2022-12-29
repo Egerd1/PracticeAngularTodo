4ab0414769e9bb921d3aaa7fdac5cb07c5fdbae1
Egerd1 <egerdvaldmaa@hot.ee>
2022-12-29 20:31:35 +0200

    Add Project to GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './models/Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'practice-angular-todo';
+export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+
+  inputTodo: string = "";
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+       {
+        content: 'Add todo',
+        completed: false
+      },
+      {
+        content: 'If ready',
+        completed: true
+      } 
+    ]
+  }
+   
+  toggleDone(id: number){
+    this.todos.map((v, i) => {
+      if(i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  deleteTodo(id: number){
+    this.todos = this.todos.filter((v, i) => i !== id);
+
+  }
+
+  addTodo(){
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
+  }
 }
